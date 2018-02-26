cd628c7e7937c7d56dab6a5cb7d7bffcd9461262
stefanhans <uni@stefan-hans.de>
2018-02-26 12:40:22 +0100

    Initialize deployment

--- main.go
+++ main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World, I'm Golang!")
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Go4k8s</title>
+</head>
+<body style="background-color:gray">
+  <h3>Hello From Go4k8s Webserver</h3>
+  <p>Version: %s</p>
+</body>
+</html>
+`
+var version = "1.0.4"
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	format := "%s - [%s] \"%s %s %s\" %s\n"
+	fmt.Printf(format, version, time.Now().Format(time.RFC1123),
+		r.Method, r.URL.Path, r.Proto, r.UserAgent())
+	fmt.Fprintf(w, html, version)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/", httpHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
